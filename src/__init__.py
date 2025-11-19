3e1106611f543b6aef8f995d1635895f99904ea6
McNamara <holgerehrmann@gmail.com>
2025-11-19 06:05:50 +0100

    Update license to GPLv3 in package metadata
    Changed the __license__ field in src/__init__.py from MIT to GPLv3 to reflect the new licensing terms for the project.

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,5 @@
 __version__ = "0.1.0"
 __author__ = "Holger Ehrmann, GFZ Data Services"
 __copyright__ = "Copyright (c) 2025 GFZ Helmholtz Centre for Geosciences"
-__license__ = "MIT"
+__license__ = "GPLv3"
 __description__ = "GFZ Repository Operations & Batch Interface - A modern GUI tool for managing DataCite DOIs"
