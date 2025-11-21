f1ee14b7a4de856c4d182ae6dff8290d03c50c35
McNamara <holgerehrmann@gmail.com>
2025-11-21 15:48:27 +0100

    Add version and metadata file
    Introduced src/__version__.py to store version, author, license, and project metadata for GROBI.

---
+++ src/__version__.py
@@ -0,0 +1,12 @@
+"""Version information for GROBI."""
+
+__version__ = "0.3.0"
+__version_info__ = (0, 3, 0)
+
+# Release information
+__author__ = "Holger Ehrmann"
+__author_email__ = "ehrmann@gfz.de"
+__organization__ = "GFZ Data Services, GFZ Helmholtz Centre for Geosciences"
+__license__ = "GNU General Public License v3.0"
+__url__ = "https://github.com/McNamara84/grobi"
+__description__ = "GFZ Research Data Repository Operations & Batch Interface"
